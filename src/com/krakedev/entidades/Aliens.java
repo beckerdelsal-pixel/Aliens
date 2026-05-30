498e3550189dbb0aebad55a7272554a675817dee
beckerdelsal-pixel <beckerdelsal@gmail.com>
2026-05-29 21:26:37 -0500

    clase alien con atributos privados parte 2

---
+++ src/com/krakedev/entidades/Aliens.java
@@ -0,0 +1,12 @@
+package com.krakedev.entidades;
+
+public class Aliens {
+	private int tamaño;
+	private String color;
+	private int numeroOjos;
+	private int numeroBrazos;
+	private int numeroPies;
+	private double precioExtremidad;
+	private double precioOjo;
+	private double precioCuerpo;
+}
