3750b13259a5128f56d7ec2cb3a223e1c18343f7
beckerdelsal-pixel <beckerdelsal@gmail.com>
2026-05-31 22:19:36 -0500

    Parte 3 terminada, agregar constructor y calculo de precios en el mismo inicializando variables

--- src/com/krakedev/entidades/Aliens.java
+++ src/com/krakedev/entidades/Aliens.java
@@ -9,4 +9,21 @@ public class Aliens {
 	private double precioExtremidad;
 	private double precioOjo;
 	private double precioCuerpo;
+	
+	public Aliens(int tamaño, String color) {
+		if(tamaño < 5) {
+			this.tamaño = 5;
+		}else if(tamaño > 30){
+			this.tamaño = 30;
+		}else {
+			this.tamaño=tamaño;
+		}
+		this.color=color;
+		this.numeroBrazos=0;
+		this.numeroOjos=0;
+		this.numeroPies=0;
+		this.precioCuerpo = this.tamaño * 0.20;      
+        this.precioExtremidad = this.tamaño * 0.10;   
+        this.precioOjo = this.tamaño * 0.05;
+	}
 }
